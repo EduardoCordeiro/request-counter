c1cbd5423bf8ad4290ae599bf1481ecb8a39bf58
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2023-09-17 17:35:00 +0900

    Split tests and small refactor

--- services/services_test.go
+++ services/services_test.go
@@ -8,7 +8,43 @@ import (
 	"time"
 )
 
-func TestParseLogLine(t *testing.T) {
+func generateLogs(times []int) []string {
+	var logs []string
+	var counter int = 0
+
+	for _, time := range times {
+		logLine := generateLogLine(counter, time)
+		logs = append(logs, logLine)
+		counter++
+	}
+
+	return logs
+}
+
+func generateLogLine(counter int, delta int) string {
+	difference := time.Duration(time.Duration(delta) * time.Second)
+	timestamp := time.Now().Add(-difference).Local().Format(time.RFC3339)
+
+	logLine := fmt.Sprintf(`{"id":%d,"timestamp":"%s"}`, counter, timestamp)
+	return logLine
+}
+
+func writeTestLogToFile(t *testing.T, filePath string, lines []string) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		t.Fatalf("Error creating test log file: %v", err)
+	}
+	defer file.Close()
+
+	for _, line := range lines {
+		_, err := file.WriteString(line + "\n")
+		if err != nil {
+			t.Fatalf("Error writing test log line: %v", err)
+		}
+	}
+}
+
+func TestParseValidLogLine(t *testing.T) {
 	// Test valid input
 	validLogLine := `{"id":1,"timestamp":"2023-09-16T16:57:18+08:00"}`
 	expectedLogLine := values.LogLine{
@@ -23,34 +59,15 @@ func TestParseLogLine(t *testing.T) {
 	if parsedLogLine != expectedLogLine {
 		t.Errorf("Expected: %+v, Got: %+v", expectedLogLine, parsedLogLine)
 	}
+}
 
+func TestParseInvalidLogLine(t *testing.T) {
 	// Test invalid input
 	invalidLogLine := `invalid JSON`
-	_, err = parseLogLine(invalidLogLine)
+	_, err := parseLogLine(invalidLogLine)
 	if err == nil {
 		t.Errorf("Expected error for invalid JSON, but got nil")
 	}
-}
-
-func generateLogLine(counter int, when int) string {
-	difference := time.Duration(time.Duration(when) * time.Second)
-	timestamp := time.Now().Add(-difference).Local().Format(time.RFC3339)
-
-	logLine := fmt.Sprintf(`{"id":%d,"timestamp":"%s"}`, counter, timestamp)
-	return logLine
-}
-
-func generateLogs(times []int) []string {
-	var logs []string
-	var counter int = 0
-
-	for _, time := range times {
-		logLine := generateLogLine(counter, time)
-		logs = append(logs, logLine)
-		counter++
-	}
-
-	return logs
 }
 
 func TestReadLogLinesInside(t *testing.T) {
@@ -92,21 +109,6 @@ func TestReadLogLinesOutside(t *testing.T) {
 	}
 	if id != 2 {
 		t.Errorf("Expected ID: 2, Got: %d", id)
-	}
-}
-
-func writeTestLogToFile(t *testing.T, filePath string, lines []string) {
-	file, err := os.Create(filePath)
-	if err != nil {
-		t.Fatalf("Error creating test log file: %v", err)
-	}
-	defer file.Close()
-
-	for _, line := range lines {
-		_, err := file.WriteString(line + "\n")
-		if err != nil {
-			t.Fatalf("Error writing test log line: %v", err)
-		}
 	}
 }
 
